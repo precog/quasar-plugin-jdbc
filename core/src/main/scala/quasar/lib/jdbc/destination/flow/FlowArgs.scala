8e2601a87a7eb763c2a7157f5d2c87d023a37dcb
Maxim Zimaliev <zimaliev@gmail.com>
2021-04-16 20:44:54 +0300

    Flow and FlowSinks

---
+++ core/src/main/scala/quasar/lib/jdbc/destination/flow/FlowArgs.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2020 Precog Data
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package quasar.lib.jdbc.destination.flow
+
+import slamdata.Predef._
+
+import slamdata.Predef._
+
+import cats.data.NonEmptyList
+import cats.implicits._
+
+import quasar.api.Column
+import quasar.api.resource.ResourcePath
+import quasar.connector.destination.{WriteMode => QWriteMode, ResultSink}, ResultSink.{UpsertSink, AppendSink}
+
+import org.slf4s.Logger
+
+sealed trait FlowArgs[T] {
+  val path: ResourcePath
+  val writeMode: QWriteMode
+  val columns: NonEmptyList[Column[T]]
+  val idColumn: Option[Column[_]]
+  val filterColumn: Option[Column[_]]
+}
+
+object FlowArgs {
+  def ofUpsert[T](upsert: UpsertSink.Args[T]): FlowArgs[T] = new FlowArgs[T] {
+    val path = upsert.path
+    val writeMode = upsert.writeMode
+    val columns = upsert.columns
+    val idColumn = upsert.idColumn.some
+    val filterColumn = upsert.idColumn.some
+  }
+
+  def ofAppend[T](append: AppendSink.Args[T]): FlowArgs[T] = new FlowArgs[T] {
+    val path = append.path
+    val writeMode = append.writeMode
+    val columns = append.columns
+    val idColumn = append.pushColumns.primary
+    val filterColumn = None
+  }
+
+  def ofCreate[T](
+      resourcePath: ResourcePath,
+      inpColumns: NonEmptyList[Column[T]])
+      : FlowArgs[T] = new FlowArgs[T] {
+    val path = resourcePath
+    val writeMode = QWriteMode.Replace
+    val columns = inpColumns
+    val idColumn = None
+    val filterColumn = None
+  }
+}
+
+
+
+
---
+++ core/src/main/scala/quasar/lib/jdbc/destination/flow/implicits.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020 Precog Data
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package quasar.lib.jdbc.destination.flow
+
+import slamdata.Predef._
+
+import cats.data._
+import cats.effect.{Effect, Sync}
+import cats.implicits._
+
+import doobie._
+import doobie.implicits._
+
+import fs2.{Pipe, Stream}
+
+import quasar.api.Column
+import quasar.api.push.OffsetKey
+import quasar.api.resource._
+import quasar.connector._
+import quasar.connector.destination.{WriteMode => QWriteMode, ResultSink}, ResultSink.{UpsertSink, AppendSink}
+import quasar.connector.render.RenderConfig
+import quasar.lib.jdbc.destination.WriteMode
+
+import org.slf4s.Logger
+
+import skolems.∀
+
+object implicits {
+  implicit class PipeLogger[F[_]: Sync, A, B, C](pipe: Pipe[F, DataEvent[C, OffsetKey.Actual[A]], B]) {
+    def withLogger(logger: Logger): Pipe[F, DataEvent[C, OffsetKey.Actual[A]], B] = { events =>
+      def trace(msg: => String) = Sync[F].delay(logger.trace(msg))
+
+      def logEvents(event: DataEvent[_, _]): F[Unit] = trace { event match {
+        case DataEvent.Create(chunk) =>
+          s"Loading chunk with size: ${chunk.size}"
+        case DataEvent.Delete(idBatch) =>
+          s"Deleting ${idBatch.size} records"
+        case DataEvent.Commit(_) =>
+          "Commit"
+      }}
+      events.evalTap(logEvents).through(pipe.logStartAndEnd(logger))
+    }
+  }
+
+  implicit class StartEndLogger[F[_]: Sync, A, B](pipe: Pipe[F, A, B]) {
+    def logStartAndEnd(logger: Logger): Pipe[F, A, B] = { events =>
+      def trace(msg: => String) = Sync[F].delay(logger.trace(msg))
+      Stream.eval_(trace("Strating load")) ++
+      events.through(pipe) ++
+      Stream.eval_(trace("Finished load"))
+    }
+  }
+}
